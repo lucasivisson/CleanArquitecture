2762d5c09324127e8dbe34310dcdd347f1bfae80
lucasivisson <lucasivisson1@gmail.com>
2022-08-08 23:33:11 -0300

    chore: add jest to lint staged

--- jest.config.ts
+++ jest.config.ts
@@ -5,18 +5,18 @@
 
 export default {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // A map from regular expressions to paths to transformers
   transform: {
-    ".+\\.ts$": "ts-jest",
-  },
+    '.+\\.ts$': 'ts-jest'
+  }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
@@ -35,4 +35,4 @@ export default {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-};
+}
